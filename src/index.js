507210d582789f9400461d7b880d2eb10ff2012f
DavidDeprost <dadeprost@gmail.com>
2018-11-08 14:53:52 +0100

    Start of major overhaul; implement gameboard as svg instead of css
    (a pure css approach doesn't allow see-through holes, so falling checkers with gsap are impossible)

--- src/index.js
+++ src/index.js
@@ -160,18 +160,140 @@ class Grid extends React.Component {
   
   render() {
     return (
-      <div>
+      <React.Fragment>
         <Status winner={this.checkForWinner()} redIsNext={this.state.redIsNext} />
-        <div id="gameGrid">
-          {
-            // This construct 'loops' to create the rows of the grid;
-            // regular looping is not available inside JSX code!
-            [...Array(this.state.rows)].map((el, i) => {
-              return this.renderRow(i);
-            })
-          }
+        <div id='gameboard'>
+          <svg id='container' width='98%' viewBox='0 0 810 785' xmlns='http://www.w3.org/2000/svg'>
+            {/* This is the gameboard container, which holds the left and right 'pillars', the top and bottom
+                paddings, and an extra invisible top row, which will show the checkers that are about to drop,
+                when hovering. */}
+                <defs>
+                  <radialGradient id='yellow' cx='50%' cy='50%' r='50%' fx='50%' fy='50%'>
+                    <stop offset='63%' stopColor='rgb(251, 255, 0)' stopOpacity='0.9' />
+                    <stop offset='100%' stopColor='rgb(255, 174, 0)' stopOpacity='1' />
+                  </radialGradient>(235, 118, 10)
+                  <radialGradient id='orange' cx='50%' cy='50%' r='50%' fx='50%' fy='50%'>
+                      <stop offset='65%' stopColor='rgb(253, 156, 0)' stopOpacity='0.65' />
+                      <stop offset='100%' stopColor='rgb(253, 101, 0)' stopOpacity='0.95' />
+                  </radialGradient>
+                  <radialGradient id='red' cx='50%' cy='50%' r='50%' fx='50%' fy='50%'>
+                      <stop offset='60%' stopColor='rgb(246, 75, 7)' stopOpacity='0.8' />
+                      <stop offset='100%' stopColor='rgb(209, 0, 7)' stopOpacity='1' />
+                  </radialGradient>
+                  <radialGradient id='green' cx='50%' cy='50%' r='50%' fx='50%' fy='50%'>
+                      <stop offset='65%' stopColor='rgb(134, 246, 7)' stopOpacity='0.8' />
+                      <stop offset='100%' stopColor='rgb(21, 139, 0)' stopOpacity='1' />
+                  </radialGradient>
+                  <radialGradient id='cyan' cx='50%' cy='50%' r='50%' fx='50%' fy='50%'>
+                      <stop offset='65%' stopColor='rgb(66, 255, 182)' stopOpacity='1' />
+                      <stop offset='100%' stopColor='rgb(15, 119, 145)' stopOpacity='1' />
+                  </radialGradient>
+                  <radialGradient id='blue' cx='50%' cy='50%' r='50%' fx='50%' fy='50%'>
+                      <stop offset='60%' stopColor='rgb(7, 210, 246)' stopOpacity='0.8' />
+                      <stop offset='100%' stopColor='rgb(0, 79, 251)' stopOpacity='1' />
+                  </radialGradient>
+                  <radialGradient id='purple' cx='50%' cy='50%' r='50%' fx='50%' fy='50%'>
+                      <stop offset='60%' stopColor='rgb(255, 84, 190)' stopOpacity='0.8' />
+                      <stop offset='100%' stopColor='rgb(159, 0, 251)' stopOpacity='1' />
+                  </radialGradient>
+
+                  <linearGradient id='blackTop' x1='0' y1='0' x2='0' y2='1'>
+                      <stop offset='20%' stopColor='rgb(54, 51, 51)' stopOpacity='0.95' />
+                      <stop offset='50%' stopColor='rgb(0, 0, 0)' stopOpacity='1' />
+                      <stop offset='90%' stopColor='rgb(54, 51, 51)' stopOpacity='0.9' /> 
+                  </linearGradient>
+                  <linearGradient id='blackBottom' x1='0' y1='0' x2='0' y2='1'>
+                      <stop offset='30%' stopColor='rgb(10, 10, 10)' stopOpacity='0.8' />
+                      <stop offset='50%' stopColor='rgb(0, 0, 0)' stopOpacity='0.9' />
+                      <stop offset='85%' stopColor='rgb(54, 51, 51)' stopOpacity='1' /> 
+                  </linearGradient>
+                  <linearGradient id='blackPillars' x1='0' y1='0' x2='1' y2='0'>
+                      <stop offset='0%' stopColor='rgb(0, 0, 0)' stopOpacity='1' />
+                      <stop offset='20%' stopColor='rgb(37, 35, 35)' stopOpacity='1' />
+                      <stop offset='35%' stopColor='rgb(54, 51, 51)' stopOpacity='1' />
+                      <stop offset='50%' stopColor='rgb(77, 72, 72)' stopOpacity='1' />
+                      <stop offset='65%' stopColor='rgb(54, 51, 51)' stopOpacity='1' />
+                      <stop offset='80%' stopColor='rgb(37, 35, 35)' stopOpacity='1' />
+                      <stop offset='100%' stopColor='rgb(0, 0, 0)' stopOpacity='1' />
+                  </linearGradient>
+                  <linearGradient id='blackGreyBlack' x1='0' y1='0' x2='1' y2='0'>
+                      <stop offset='0%' stopColor='rgb(0, 0, 0)' stopOpacity='1' />
+                      <stop offset='12%' stopColor='rgb(15, 15, 15)' stopOpacity='0.9' />
+                      <stop offset='35%' stopColor='rgb(30, 30, 30)' stopOpacity='0.85' />
+                      <stop offset='50%' stopColor='rgb(50, 50, 50)' stopOpacity='0.80' />
+                      <stop offset='65%' stopColor='rgb(30, 30, 30)' stopOpacity='0.85' />
+                      <stop offset='88%' stopColor='rgb(15, 15, 15)' stopOpacity='0.9' />
+                      <stop offset='100%' stopColor='rgb(0, 0, 0)' stopOpacity='1' />
+                  </linearGradient>
+                </defs>
+              
+              <svg id='gameGrid' width='700' height='640' x='57' y='115' xmlns='http://www.w3.org/2000/svg'>
+                 {/* This is the actual gameboard consisting of 6 rows x 7 columns of square 100px x 100px cells;
+                    this means the board itself is 600px by 700px with each column being 600 by 100px. */}
+                <defs>
+                  <pattern id='hole' patternUnits='userSpaceOnUse' width='100' height='100'>
+                    {/* The <pattern> matches the size of a cell, 100x100, and contains a <circle>,
+                        representing the hole, that matches the size of the checker.
+                        The <circle> gets a fill color of 'black'; in the context of a <mask>, this
+                        means the absence of space, or full transparency, as opposed to literal black. */}
+                    <circle cx='50' cy='50' r='40' fill='black'></circle>
+                  </pattern>
+
+                  <mask id='cell-mask'>
+                    {/* The <mask> is composed of two <rect> elements that match the grid column size;
+                        the first gets a fill color of 'white' (opposite of 'black' in a mask) to represent
+                        the part of the column we want to be opaque/visible.
+                        The second <rect> sits on top of the first and has a fill of url(#hole) which refers
+                        to the pattern we created above. */}
+                    <rect width='100' height='600' fill='white'></rect>
+                    <rect width='100' height='600' fill='url(#hole)'></rect>
+                  </mask>
+                </defs>
+
+                {/* Now, we can set the mask attribute for our grid column <rect>, by referencing
+                    the <mask> element by id: 'url(#cell-mask)'. A nice feature of the <pattern> element
+                    is that it repeats itself, based on the height/width attributes we've provided.
+                    This means we can reveal the 6 rows of a single column without adding each
+                    circular hole to the DOM explicitly. To build multiple columns, we simply add
+                    a nested <svg> element at the correct x position to wrap each masked <rect>! */}
+                <svg id='column1' x='0' y='0'>
+                  <rect width='100' height='600' fill='url(#blackGreyBlack)' mask='url(#cell-mask)' />
+                </svg>
+
+                <svg id='column2' x='99' y='0'>
+                  <rect width='100' height='600' fill='url(#blackGreyBlack)' mask='url(#cell-mask)' />
+                </svg>
+
+                <svg id='column3' x='198' y='0'>
+                  <rect width='100' height='600' fill='url(#blackGreyBlack)' mask='url(#cell-mask)' />
+                </svg>
+
+                <svg id='column4' x='297' y='0'>
+                  <rect width='100' height='600' fill='url(#blackGreyBlack)' mask='url(#cell-mask)' />
+                </svg>
+
+                <svg id='column5' x='396' y='0'>
+                  <rect width='100' height='600' fill='url(#blackGreyBlack)' mask='url(#cell-mask)' />
+                </svg>
+                
+                <svg id='column6' x='495' y='0'>
+                  <rect width='100' height='600' fill='url(#blackGreyBlack)' mask='url(#cell-mask)' />
+                </svg>
+
+                <svg id='column7' x='594' y='0'>
+                  <rect width='106' height='600' fill='url(#blackGreyBlack)' mask='url(#cell-mask)' />
+                </svg>
+
+              </svg>
+              
+            
+            <rect id='bottom-padding' width='700' height='20' x='51' y='712' fill='url(#blackBottom)' />
+            <rect id='left-pillar' width='60' height='685' fill='url(#blackPillars)' x='0' y='100' rx='10' ry='10' />
+            <rect id='right-pillar' width='60' height='685' fill='url(#blackPillars)' x='750' y='100' rx='10' ry='10' />
+            <rect id='top-padding' width='710' height='20' x='48' y='100' fill='url(#blackTop)' />
+          </svg>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
