88681896e78051cb27d98550171a5b3385630b21
DavidDeprost <dadeprost@gmail.com>
2019-09-24 04:10:24 +0200

    Updates

---
+++ precache-manifest.5bd3a07fe55d47ea72db412de8eb7ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d5c8f5b4c04ac59e888dd4d1ef5712",
+    "url": "/connect4_react/index.html"
+  },
+  {
+    "revision": "a21f3f3261538d7d7e40",
+    "url": "/connect4_react/static/css/main.13d6f63f.chunk.css"
+  },
+  {
+    "revision": "dd60f703c4eab7ab4baa",
+    "url": "/connect4_react/static/js/2.816496e5.chunk.js"
+  },
+  {
+    "revision": "a21f3f3261538d7d7e40",
+    "url": "/connect4_react/static/js/main.3af4f923.chunk.js"
+  },
+  {
+    "revision": "6f8a4a5b83a9c024653e",
+    "url": "/connect4_react/static/js/runtime~main.93a6b071.js"
+  }
+]);
